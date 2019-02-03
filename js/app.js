1488ae7c1546c20274f5892612a47ed74df49811
Hasina Akhter <hakhter2002@yahoo.com>
2019-02-03 15:29:50 -0500

    Update app.js
    updated info

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 $(function(){
 $(".fall").mouseenter(function(){
-	$(".fall1 ").html("<b>A single page website with a full-screen landing page<br>Built from scratch with html5,css3,jQuery and php<br>Optimized to different devices</b>");
+	$(".fall1 ").html("<b>A single page responsive site with a full-screen landing page<br>Built with html5,css3,jQuery</b>");
     $(".fall1 " ).css("font-family" ,"Bree serif");
 });
 $(".fall").mouseleave(function(){
 	$(".fall1").text("taughannockfalls");	
 });
 $(".guide").mouseenter(function(){
-	$(".guide1 ").html("<b>A multi-page website <br> Built from scratch with html5,css3,jQuery and php<br>Optimized to different devices</b>");
+	$(".guide1 ").html("<b>A multi-page responsive site <br> Built on Bootstrap with a subcription form built in php</b>");
     $(".guide1 ").css("font-family" ,"Bree serif");
 });	
 $(".guide").mouseleave(function(){
 	$(".guide1").text("agoodlifeguide");	
 });
+$(".app").mouseenter(function(){
+	$(".app1 ").html("<b>A todo tracker built on Bootswatch and Javascript <br>Optimized to different devices</b>");
+    $(".app1 ").css("font-family" ,"Bree serif");
+});	
+$(".app").mouseleave(function(){
+	$(".app1").text("todo app");	
+});
 $(".photogallery").mouseenter(function(){
-	$(".photo1 ").html("<b>A website with a collection of photos<br> Built from scratch with html5,css3 and jQuery<br>Optimized to different devices</b>");
+	$(".photo1 ").html("<b>A responsive website of a collection of photos<br> Built with html5,css3 and jQuery</b>");
     $(".photo1 ").css("font-family" ,"Bree serif");
 });
 $(".photogallery").mouseleave(function(){
